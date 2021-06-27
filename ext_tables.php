07b88987ad2b7824cbc2249207dbaa6f77e90796
dschuesslerC3 <schuessler@myc3.com>
2021-06-27 20:21:42 +0200

    added filelist styles + added default rgba colors to description

--- ext_tables.php
+++ ext_tables.php
@@ -12,37 +12,37 @@ if (TYPO3_MODE == 'BE') {
         'type' => 'check',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_1'] = [
-        'label' => 'Custom Color 1',
+        'label' => 'Custom Color 1 - default of dark skin: rgb(21, 21,21)',
         'type' => 'text',
         'default' => 'rgb(21, 21,21)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_2'] = [
-        'label' => 'Custom Color 2',
+        'label' => 'Custom Color 2 - default of dark skin: rgb(41, 41, 41)',
         'type' => 'text',
         'default' => 'rgb(41, 41, 41)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_4'] = [
-        'label' => 'Custom Color 4',
+        'label' => 'Custom Color 4 - default of dark skin: rgb(60, 63, 65)',
         'type' => 'text',
         'default' => 'rgb(60, 63, 65)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_5'] = [
-        'label' => 'Custom Color 5',
+        'label' => 'Custom Color 5 - default of dark skin: rgb(245, 245, 245)',
         'type' => 'text',
         'default' => 'rgb(245, 245, 245)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_6'] = [
-        'label' => 'Custom Color 6',
+        'label' => 'Custom Color 6 - default of dark skin: rgba(245, 245, 245, 0.3)',
         'type' => 'text',
         'default' => 'rgba(245, 245, 245, 0.3)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_7'] = [
-        'label' => 'Custom Color 7',
+        'label' => 'Custom Color 7 - default of dark skin: rgb(31, 31, 31)',
         'type' => 'text',
         'default' => 'rgb(31, 31, 31)',
     ];
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_skins_custom_color_8'] = [
-        'label' => 'Custom Color 8',
+        'label' => 'Custom Color 8 - default of dark skin: rgb(253, 195, 0)',
         'type' => 'text',
         'default' => 'rgb(253, 195, 0)',
     ];
