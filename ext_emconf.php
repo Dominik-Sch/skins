5b8cf6ec56785a491e08a1d8ef10a4c33d29a9b3
Dominik Schüßler <dominikschuessler1337@gmail.com>
2023-08-05 11:32:29 +0200

    raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,12 @@
 <?php
 
+/** @var string $_EXTKEY */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 Skins',
     'description' => 'This extension adds a dark skin (dark theme) to the TYPO3 backend. The colors are customizable and can be changed to your favorite colors.',
     'category' => 'be',
-    'version' => '11.0.0',
+    'version' => '11.0.1',
     'state' => 'stable',
     'uploadfolder' => false,
     'clearcacheonload' => true,
