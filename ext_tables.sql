690b4f025e8f53a3de24fa0cb60efab0656c6a6b
Dominik Schüßler <dominikschuessler1337@gmail.com>
2023-08-15 22:33:03 +0200

    set default value for settings db field

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,6 @@
 #
 CREATE TABLE be_users
 (
-    tx_skins_active             int(1) DEFAULT 0 NOT NULL,
-    tx_skins_dark_mode_settings text             NOT NULL
+    tx_skins_active             int(1) DEFAULT 0    NOT NULL,
+    tx_skins_dark_mode_settings text   DEFAULT '{}' NOT NULL
 );
